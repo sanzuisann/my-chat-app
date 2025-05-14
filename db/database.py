d50bd516ec5618579d93b73310bb818ce48e042e
sanzuisann <sanzuisann0122@gmail.com>
2025-05-14 20:47:41 +0900

    Add models and database setup

--- db/database.py
+++ db/database.py
@@ -2,9 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-# 環境変数からDB URLを取得
+# DATABASE_URL は Render の Environment Variables に登録されているはずです
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# 接続エンジンとセッション作成
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
