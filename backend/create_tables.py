2301c9bb248bb0a2d088b0cbc60b1fef2154afe4
sanzuisann <sanzuisann0122@gmail.com>
2025-06-11 00:10:50 +0900

    Load .env before creating tables

--- backend/create_tables.py
+++ backend/create_tables.py
@@ -1,3 +1,9 @@
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
 from backend.db.database import engine
 from backend.models.models import Base
 
